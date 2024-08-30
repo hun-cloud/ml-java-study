f26f9a46a1cec4b107dbacac983d9657462a23c9
leejaehun <ljh0901@mlsoft.com>
2024-08-30 15:44:45 +0900

    init

---
+++ src/main/java/com/study/mlsoft/MlsoftApplication.java
@@ -0,0 +1,13 @@
+package com.study.mlsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlsoftApplication.class, args);
+	}
+
+}
